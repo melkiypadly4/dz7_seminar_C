5774fe8bd08c5367356307fbc750d6f74ebd86de
Nikolai <djaknvn@gmail.com>
2023-01-03 21:05:12 +0300

    дз задача 1 сделано

---
+++ zad1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void ShowArray( double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+
+    double[,] array = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(leftRange, rightRange) + rand.NextDouble();
+
+            array[i, j] = Math.Round(array[i, j], 2);
+        }
+    }
+    return array;
+}
+int quantityrows = 3;
+
+int quantitycolumns = 4;
+
+double[,] matrix = CreateRandomArray(quantitycolumns, quantityrows , 1, 10);
+
+ShowArray(matrix);
